ccd9916b4647e2ceac74a6c3f22f049acbd1855b
Sarah <vanwars@gmail.com>
2024-01-27 14:14:49 -0500

    Lab 1 Completed

---
+++ labs/lab01/font-switcher/index.js
@@ -0,0 +1,11 @@
+function makeBigger() {
+    document.querySelector("h1").style.fontSize = "3em";
+    document.querySelector(".content").style.fontSize = "2em";
+    // console.log("make bigger");
+}
+
+function makeSmaller() {
+    document.querySelector("h1").style.fontSize = "1.5em";
+    document.querySelector(".content").style.fontSize = "1em";
+    // console.log("make smaller");
+}
