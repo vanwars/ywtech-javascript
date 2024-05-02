70e7d56e14309542d1ffcbd9399e0956eb806320
Sarah <vanwars@gmail.com>
2024-05-02 18:06:15 -0400

    updates

--- intro-to-react/practice/Profile.jsx
+++ intro-to-react/practice/Profile.jsx
@@ -3,34 +3,27 @@ import React, { useState } from "react";
 
 export default function Profile({ name, picture, bio }) {
     // New concept this week: state variables:
-    const [fullscreen, setFullscreen] = useState(false);
+    const [activeClass, setActiveClass] = useState("profile");
 
-    function toggle() {
-        if (fullscreen) {
-            setFullscreen(false);
-        } else {
-            setFullscreen(true);
-        }
-        // setFullscreen(!fullscreen);
+    function showFullscreen() {
+        // will redraw the screen in "full screen" mode
+        setActiveClass("profile full-screen");
     }
 
-    if (fullscreen) {
-        return (
-            <section className="profile full-screen">
-                <img src={picture} />
-                <h3>{name}</h3>
-                <div>{bio}</div>
-                <button onClick={toggle}>Back</button>
-            </section>
-        );
-    } else {
-        return (
-            <section className="profile">
-                <img src={picture} />
-                <h3>{name}</h3>
-                <div>{bio}</div>
-                <button onClick={toggle}>Fullscreen</button>
-            </section>
-        );
+    function showRegular(event) {
+        // will redraw the screen in "regular" mode
+        event.stopPropagation();
+        setActiveClass("profile");
     }
+
+    return (
+        <section className={activeClass} onClick={showFullscreen}>
+            <img src={picture} />
+            <h3>{name}</h3>
+            <div>{bio}</div>
+            <button id="regular" onClick={showRegular}>
+                Back
+            </button>
+        </section>
+    );
 }
